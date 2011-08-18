2da2b19112ea903d75dc42e5f502fe88f78eb094
Chad Mazzola <cmazzola@thoughtbot.com>
2011-08-18 10:26:07 -0400

    Fix name of file in comments.

--- generate-bourbon.sh
+++ generate-bourbon.sh
@@ -4,10 +4,10 @@
 # This will generate a bourbon directory and convert all .css.scss to .scss extensions.
 # The bourbon directory is for 'sass --watch' use outside of rails.
 # Step 1: Make install executable by changing permission
-#     chmod a+x generate-sass.sh
+#     chmod a+x generate-bourbon.sh
 
 # Step 2: Generate Files
-#     ./generate-sass.sh
+#     ./generate-bourbon.sh
 
 echo Creating directory...
 mkdir -p $PWD/bourbon/lib
