89a1e19bbf5ff862b6713967a6f80d3aca69c8fe
Gabe Berke-Williams and Phil LaPier <support+gabe+phil@thoughtbot.com>
2012-01-20 11:43:02 -0500

    Clean up cucumber feature.

--- features/step_definitions/bourbon_steps.rb
+++ features/step_definitions/bourbon_steps.rb
@@ -2,3 +2,21 @@ When /^I set up bourbon$/ do
   write_file("Gemfile", "gem 'bourbon', :path => '../../..'")
   run_simple("bundle install")
 end
+
+When /^I generate bourbon files$/ do
+  run_simple("bundle exec bourbon generate")
+end
+
+Then /^the sass directories should have been generated$/ do
+  sass_directories = ["bourbon/addons", "bourbon/css3", "bourbon/functions"]
+  check_directory_presence(sass_directories, true)
+end
+
+Then /^the master bourbon partial should have been generated$/ do
+  check_file_presence(["bourbon/_bourbon.scss"], true)
+end
+
+Then /^the lib files should have been generated$/ do
+  check_file_presence(["bourbon/lib/sass_extensions.rb"], true)
+  check_directory_presence(["bourbon/lib/sass_extensions"], true)
+end
