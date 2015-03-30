8501722bafad2e27736b208f99302540608b9e7c
Joshua Ogle <joshua@thoughtbot.com>
2015-03-30 12:34:45 -0600

    Add tests for word-wrap

---
+++ spec/bourbon/addons/word_wrap_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe "word-wrap" do
+  before(:all) do
+    ParserSupport.parse_file("addons/word-wrap")
+  end
+
+  context "called on element" do
+    it "adds word-wrap" do
+      input = ".word-wrap"
+      ruleset = "overflow-wrap: break-word; " +
+                "word-wrap: break-word; " +
+                "word-break: break-all;"
+
+      expect(input).to have_ruleset(ruleset)
+    end
+  end
+
+  context "called on element with break" do
+    it "adds break" do
+      input = ".word-wrap-break"
+      ruleset = "overflow-wrap: normal; " +
+                "word-wrap: normal; " +
+                "word-break: normal;"
+
+      expect(input).to have_ruleset(ruleset)
+    end
+  end
+end
