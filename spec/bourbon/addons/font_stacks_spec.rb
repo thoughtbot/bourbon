f4b64b7d935026b8cd1fe809a059f361633c183f
Joshua Ogle <joshua@thoughtbot.com>
2015-03-30 10:49:48 -0600

    Add test for font-stacks

---
+++ spec/bourbon/addons/font_stacks_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe "font-stacks" do
+  before(:all) do
+    ParserSupport.parse_file("addons/font-stacks")
+  end
+
+  context "stacks used in variable" do
+    it "output stacks" do
+      georgia = '"Georgia", "Cambria", "Times New Roman", "Times", serif'
+      helvetica = '"Helvetica Neue", "Helvetica", "Roboto", ' +
+                  '"Arial", sans-serif'
+      lucida_grande = '"Lucida Grande", "Tahoma", "Verdana", ' +
+                  '"Arial", sans-serif'
+      monospace = '"Bitstream Vera Sans Mono", "Consolas", "Courier", monospace'
+      verdana = '"Verdana", "Geneva", sans-serif'
+
+      expect(".georgia").to have_rule("font-family: #{georgia}")
+      expect(".helvetica").to have_rule("font-family: #{helvetica}")
+      expect(".lucida-grande").to have_rule("font-family: #{lucida_grande}")
+      expect(".monospace").to have_rule("font-family: #{monospace}")
+      expect(".verdana").to have_rule("font-family: #{verdana}")
+    end
+  end
+end
