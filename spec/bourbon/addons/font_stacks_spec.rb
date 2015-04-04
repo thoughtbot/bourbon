dedaefe4a28c0544fd9a11fe129380921b31b0fe
Tyson Gach <tyson@thoughtbot.com>
2015-04-04 12:57:12 -0400

    Modernize and rename font stacks
    - Closes #709

--- spec/bourbon/addons/font_stacks_spec.rb
+++ spec/bourbon/addons/font_stacks_spec.rb
@@ -7,19 +7,29 @@ describe "font-stacks" do
 
   context "stacks used in variable" do
     it "output stacks" do
-      georgia = '"Georgia", "Cambria", "Times New Roman", "Times", serif'
-      helvetica = '"Helvetica Neue", "Helvetica", "Roboto", ' +
-                  '"Arial", sans-serif'
-      lucida_grande = '"Lucida Grande", "Tahoma", "Verdana", ' +
-                      '"Arial", sans-serif'
-      monospace = '"Bitstream Vera Sans Mono", "Consolas", "Courier", monospace'
+      helvetica = '"Helvetica Neue", "Helvetica", "Arial", sans-serif'
+      lucida_grande = '"Lucida Grande", "Lucida Sans Unicode", ' +
+                      '"Lucida Sans", "Geneva", "Verdana", sans-serif'
       verdana = '"Verdana", "Geneva", sans-serif'
+      garamond = '"Garamond", "Baskerville", "Baskerville Old Face", ' +
+                 '"Hoefler Text", "Times New Roman", serif'
+      georgia = '"Georgia", "Times", "Times New Roman", serif'
+      hoefler_text = '"Hoefler Text", "Baskerville Old Face", ' +
+                     '"Garamond", "Times New Roman", serif'
+      consolas = '"Consolas", "monaco", monospace'
+      courier_new = '"Courier New", "Courier", "Lucida Sans Typewriter", ' +
+                    '"Lucida Typewriter", monospace'
+      monaco = '"monaco", "Consolas", "Lucida Console", monospace'
 
-      expect(".georgia").to have_rule("font-family: #{georgia}")
       expect(".helvetica").to have_rule("font-family: #{helvetica}")
       expect(".lucida-grande").to have_rule("font-family: #{lucida_grande}")
-      expect(".monospace").to have_rule("font-family: #{monospace}")
       expect(".verdana").to have_rule("font-family: #{verdana}")
+      expect(".garamond").to have_rule("font-family: #{garamond}")
+      expect(".georgia").to have_rule("font-family: #{georgia}")
+      expect(".hoefler-text").to have_rule("font-family: #{hoefler_text}")
+      expect(".consolas").to have_rule("font-family: #{consolas}")
+      expect(".courier-new").to have_rule("font-family: #{courier_new}")
+      expect(".monaco").to have_rule("font-family: #{monaco}")
     end
   end
 end
