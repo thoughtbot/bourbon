d9a2fa33e8a16f474e46fdc1a14e88e274530d41
Andrew Ross <andrew@vermonster.com>
2015-05-16 16:15:49 -0400

    Address hound comments

--- spec/bourbon/addons/sibling_count_spec.rb
+++ spec/bourbon/addons/sibling_count_spec.rb
@@ -7,28 +7,28 @@ describe "sibling-count" do
 
   context "called with a number" do
     it "applies styles to elements with specific numbers of siblings" do
-      rule = "color: white"
+      rule = "color: #ffffff"
 
-      expect(".color-varies-by-sibling-count:nth-child(1):nth-last-child(1)").to have_rule(rule)
+      expect("li:nth-child(1):nth-last-child(1)").to have_rule(rule)
     end
   end
 
   context "called with a multiple" do
     it "applies styles to elements with specific multiples of siblings" do
-      rule = "color: gray"
+      rule = "color: #808080"
 
-      expect(".color-varies-by-sibling-count:nth-child(2n+1):nth-last-child(2n+0)").to have_rule(rule)
-      expect(".color-varies-by-sibling-count:nth-child(2n+2):nth-last-child(2n+1)").to have_rule(rule)
+      expect("li:nth-child(2n+1):nth-last-child(2n+0)").to have_rule(rule)
+      expect("li:nth-child(2n+2):nth-last-child(2n+1)").to have_rule(rule)
     end
   end
 
   context "called with a multiple and an offset" do
-    it "applies styles to elements where number of siblings % multiple == offset" do
-      rule = "color: black"
+    it "applies styles when number of siblings % multiple == offset" do
+      rule = "color: #000000"
 
-      expect(".color-varies-by-sibling-count:nth-child(3n+1):nth-last-child(3n+1)").to have_rule(rule)
-      expect(".color-varies-by-sibling-count:nth-child(3n+2):nth-last-child(3n+0)").to have_rule(rule)
-      expect(".color-varies-by-sibling-count:nth-child(3n+3):nth-last-child(3n+2)").to have_rule(rule)
+      expect("li:nth-child(3n+1):nth-last-child(3n+1)").to have_rule(rule)
+      expect("li:nth-child(3n+2):nth-last-child(3n+0)").to have_rule(rule)
+      expect("li:nth-child(3n+3):nth-last-child(3n+2)").to have_rule(rule)
     end
   end
 end
