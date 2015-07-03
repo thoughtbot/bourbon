f95f95d6d17d0b6afbf3606c149cb95f33cc65db
Adriano Bonat <adrianob@gmail.com>
2015-07-02 23:36:11 -0300

    Add support to @font-face local references

--- spec/bourbon/helpers/font_source_declaration_spec.rb
+++ spec/bourbon/helpers/font_source_declaration_spec.rb
@@ -8,10 +8,10 @@ describe "font-source-declaration" do
   context "called with pipeline" do
     it "returns pipeline path" do
       rule = 'src: font-url("b.eot?#iefix") format("embedded-opentype"), ' +
-             'font-url("b.woff2") format("woff2"), ' +
-             'font-url("b.woff") format("woff"), ' +
-             'font-url("b.ttf") format("truetype"), ' +
-             'font-url("b.svg#a") format("svg")'
+        'font-url("b.woff2") format("woff2"), ' +
+        'font-url("b.woff") format("woff"), ' +
+        'font-url("b.ttf") format("truetype"), ' +
+        'font-url("b.svg#a") format("svg")'
       expect(".has-pipeline").to have_rule(rule)
     end
   end
@@ -19,11 +19,24 @@ describe "font-source-declaration" do
   context "called with no pipeline" do
     it "does not return pipeline path" do
       rule = 'src: url("b.eot?#iefix") format("embedded-opentype"), ' +
-             'url("b.woff2") format("woff2"), ' +
-             'url("b.woff") format("woff"), ' +
-             'url("b.ttf") format("truetype"), ' +
-             'url("b.svg#a") format("svg")'
+        'url("b.woff2") format("woff2"), ' +
+        'url("b.woff") format("woff"), ' +
+        'url("b.ttf") format("truetype"), ' +
+        'url("b.svg#a") format("svg")'
       expect(".no-pipeline").to have_rule(rule)
+    end
+  end
+
+  context "called with local font references" do
+    it "includes local references" do
+      rule = 'src: local("FontA"), ' +
+        'local("Font A"), ' +
+        'url("b.eot?#iefix") format("embedded-opentype"), ' +
+        'url("b.woff2") format("woff2"), ' +
+        'url("b.woff") format("woff"), ' +
+        'url("b.ttf") format("truetype"), ' +
+        'url("b.svg#a") format("svg")'
+      expect(".font-with-local-references").to have_rule(rule)
     end
   end
 end
