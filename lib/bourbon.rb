73b09f618bd3189bf5af6f639e3df07680ed4fc7
Tyson Gach <tyson@thoughtbot.com>
2016-02-05 21:44:11 -0500

    Remove engine, Railtie, and install task
    This is an artifact left from the days before the Rails Assets Pipeline. Moving
    forward, this option of install will no longer be available and we'll only
    support Rails 4.2+.

--- lib/bourbon.rb
+++ lib/bourbon.rb
@@ -1,20 +1,3 @@
 require "bourbon/generator"
-
-module Bourbon
-  if defined?(Rails) && defined?(Rails::Engine)
-    class Engine < ::Rails::Engine
-      require "bourbon/engine"
-    end
-
-    module Rails
-      class Railtie < ::Rails::Railtie
-        rake_tasks do
-          load "tasks/install.rake"
-        end
-      end
-    end
-  else
-    bourbon_path = File.expand_path("../../core", __FILE__)
-    ENV["SASS_PATH"] = [ENV["SASS_PATH"], bourbon_path].compact.join(File::PATH_SEPARATOR)
-  end
-end
+bourbon_path = File.expand_path("../../core", __FILE__)
+ENV["SASS_PATH"] = File.join([ENV["SASS_PATH"], bourbon_path].compact)
--- lib/bourbon/engine.rb
+++
@@ -1,5 +0,0 @@
-module Bourbon
-  class Engine < Rails::Engine
-    config.assets.paths << File.expand_path("../../../core", __FILE__)
-  end
-end
