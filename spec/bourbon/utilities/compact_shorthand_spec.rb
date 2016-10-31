611b585c1945cb7a0ba4bf5d18507291e1914a9b
Tyson Gach <tyson@tysongach.com>
2016-10-31 09:38:38 -0400

    Improve compact-shorthand specs

--- spec/bourbon/utilities/compact_shorthand_spec.rb
+++ spec/bourbon/utilities/compact_shorthand_spec.rb
@@ -6,20 +6,25 @@ describe "compact-shorthand" do
   end
 
   context "compact-shorthand" do
-    it "returns four distinct lengths unaltered" do
-      expect(".four").to have_rule("padding: 10px 20px 30px 40px")
+    it "returns four values unaltered" do
+      expect(".four-values-a").to have_rule("padding: 10px 20px 30px 40px")
     end
 
-    it "returns collapsed horizontal lengths" do
-      expect(".three").to have_rule("padding: 5px 10px 5px 20px")
+    it "returns four values when the left and right values are not equal" do
+      expect(".four-values-b").to have_rule("padding: 5px 10px 5px 20px")
     end
 
-    it "returns collapsed vertical and horizontal lengths" do
-      expect(".two").to have_rule("padding: 50px 100px")
+    it "compacts four values to two values when the top/bottom and " +
+       "left/right values are equal" do
+      expect(".two-values").to have_rule("padding: 50px 100px")
     end
 
-    it "returns collapsed lengths when all match" do
-      expect(".one").to have_rule("padding: 10px")
+    it "compacts four values to one value when they all match" do
+      expect(".one-value").to have_rule("padding: 10px")
+    end
+
+    it "skips null values" do
+      expect(".null-value").to have_rule("padding: 10px 20px")
     end
   end
 end
