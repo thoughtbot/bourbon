caa7f2041a32d74d461990c450c481472ea2f702
Tyson Gach <tyson@tysongach.com>
2017-07-02 12:21:49 -0400

    Add tests for gamma function
    Closes https://github.com/thoughtbot/bourbon/issues/963

---
+++ spec/bourbon/utilities/gamma_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe "gamma" do
+  before(:all) do
+    ParserSupport.parse_file("utilities/gamma")
+  end
+
+  context "called on a color channel" do
+    it "outputs a gamma value between 0 and 1" do
+      rule = "content: 0.13185;"
+
+      expect(".gamma").to have_ruleset(rule)
+    end
+  end
+
+  context "called on a full color channel" do
+    it "outputs a gamma value between 0 and 1" do
+      rule = "content: 1;"
+
+      expect(".gamma-full").to have_ruleset(rule)
+    end
+  end
+end
