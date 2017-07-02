9130dea374f22227c4f4ab04a8480ba85568d10a
Tyson Gach <tyson@tysongach.com>
2017-07-02 11:46:41 -0400

    Add tests for lightness function
    Closes https://github.com/thoughtbot/bourbon/issues/964

---
+++ spec/bourbon/utilities/lightness_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe "lightness" do
+  before(:all) do
+    ParserSupport.parse_file("utilities/lightness")
+  end
+
+  context "called on black" do
+    it "outputs a number between 0 and 1 to indicate lightness" do
+      rule = "content: 0;"
+
+      expect(".lightness-black").to have_ruleset(rule)
+    end
+  end
+
+  context "called on white" do
+    it "outputs a number between 0 and 1 to indicate lightness" do
+      rule = "content: 1;"
+
+      expect(".lightness-white").to have_ruleset(rule)
+    end
+  end
+
+  context "called on gray" do
+    it "outputs a number between 0 and 1 to indicate lightness" do
+      rule = "content: 0.21795;"
+
+      expect(".lightness-gray").to have_ruleset(rule)
+    end
+  end
+end
