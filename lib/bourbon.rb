9bedac008832cb779ded2f9298f12a5ebae9e88a
Prem Sichanugrist <s@sikac.hu>
2019-07-08 16:51:23 -0400

    Remove sass dependency
    Given that Ruby sass is now deprecated and developers are suggested to
    move to sassc, we should stop specifying sass as a runtime dependency.
    
    Also, since it seems like we required sass only just to add to
    load_paths, it should be safe to silently fail if it is not available.

--- lib/bourbon.rb
+++ lib/bourbon.rb
@@ -1,12 +1,14 @@
-require "sass"
 require "bourbon/generator"
 
 module Bourbon
   if defined?(Rails) && defined?(Rails::Engine)
     class Engine < ::Rails::Engine
-      config.assets.paths << File.expand_path("../../core", __FILE__)
+      config.assets.paths << File.expand_path("../core", __dir__)
     end
   else
-    Sass.load_paths << File.expand_path("../../core", __FILE__)
+    begin
+      require "sass"
+      Sass.load_paths << File.expand_path("../core", __dir__)
+    rescue LoadError; end
   end
 end
