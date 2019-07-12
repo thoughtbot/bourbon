851a96d4b7d77cd9898b1bcfcaa70e60514a8c3d
Tyson Gach <tyson@tysongach.com>
2019-07-12 14:19:06 -0400

    Disable sourcemaps in tests

--- spec/support/sass_support.rb
+++ spec/support/sass_support.rb
@@ -1,7 +1,7 @@
 module SassSupport
   def generate_css
     FileUtils.mkdir("tmp")
-    `sass -I . spec/fixtures:tmp --update --quiet --precision=5`
+    `sass -I . spec/fixtures:tmp --update --quiet --precision=5 --sourcemap=none`
   end
 
   def clean_up
